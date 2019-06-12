03b937f4daddd0216a881236bee4ac91b15a5f8b
ItzLevvie <rsajeey@hotmail.com>
2019-06-12 13:34:32 +0100

    Merge fixes from servicing/1906/190612-1332

--- launch.sh
+++ launch.sh
@@ -1,19 +1,21 @@
-node sources/Dogfood_DevMain.js > defconfig
-node sources/Dogfood_CC.js >> defconfig
-node sources/Dogfood_FRDC.js >> defconfig
-node sources/Dogfood_DCEXT.js >> defconfig
-echo >> defconfig
-node sources/Microsoft_DevMain.js >> defconfig
-node sources/Microsoft_CC.js >> defconfig
-node sources/Microsoft_FRDC.js >> defconfig
-node sources/Microsoft_DC.js >> defconfig
-node sources/Microsoft_LTSC.js >> defconfig
-echo >> defconfig
-node sources/Insiders_DevMain.js >> defconfig
-node sources/Insiders_CC.js >> defconfig
-node sources/Insiders_FRDC.js >> defconfig
-node sources/Insiders_LTSC.js >> defconfig
-echo >> defconfig
-node sources/Production_CC.js >> defconfig
-node sources/Production_DC.js >> defconfig
-node sources/Production_LTSC.js >> defconfig
+{
+    node sources/Dogfood_DevMain.js
+    node sources/Dogfood_CC.js
+    node sources/Dogfood_FRDC.js
+    node sources/Dogfood_DCEXT.js
+    echo ''
+    node sources/Microsoft_DevMain.js
+    node sources/Microsoft_CC.js
+    node sources/Microsoft_FRDC.js
+    node sources/Microsoft_DC.js
+    node sources/Microsoft_LTSC.js
+    echo ''
+    node sources/Insiders_DevMain.js
+    node sources/Insiders_CC.js
+    node sources/Insiders_FRDC.js
+    node sources/Insiders_LTSC.js
+    echo ''
+    node sources/Production_CC.js
+    node sources/Production_DC.js
+   node sources/Production_LTSC.js
+} > defconfig
