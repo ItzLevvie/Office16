f905d7d422a1c531816fdbca9d9b9c5a3d5b5369
ItzLevvie <rsajeey@hotmail.com>
2019-12-01 18:54:23 +0000

    20

--- sources/Microsoft_LTSC.js
+++ sources/Microsoft_LTSC.js
@@ -8,9 +8,9 @@ https.get(`https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/
         if (getResponse.statusCode === 200) {
             const getParsedData = JSON.parse(getData);
             const getBuild = getParsedData.AvailableBuild;
-            console.log(`Microsoft_LTSC : ${getFFN} : ${getBuild}`);
+            console.log(`Microsoft_LTSC    : ${getFFN} : ${getBuild}`);
         } else {
-            console.log(`Microsoft_LTSC : ${getFFN} : ${getBuild}`);
+            console.log(`Microsoft_LTSC    : ${getFFN} : ${getBuild}`);
         };
     });
 });
