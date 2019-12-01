016006dd9e27d0e5b9ba16b2ffbd082ad2eb9a4f
ItzLevvie <rsajeey@hotmail.com>
2019-12-01 18:10:59 +0000

    9

--- sources/Insiders_LTSC.js
+++ sources/Insiders_LTSC.js
@@ -1,16 +1,16 @@
 const https = require("https");
 
-const getFFN = "492350f6-3a01-4f97-b9c0-c7c6ddf67d60";
-const getBuild = "16.0.12130.20410";
+const getFFN = "2e148de9-61c8-4051-b103-4af54baffbb4";
+const getBuild = "16.0.10351.20054";
 
 https.get(`https://mrodevicemgr.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=${getFFN}`, (getResponse) => {
     getResponse.on("data", (getData) => {
         if (getResponse.statusCode === 200) {
             const getParsedData = JSON.parse(getData);
             const getBuild = getParsedData.AvailableBuild;
-            console.log(` : ${getFFN} : ${getBuild}`);
+            console.log(`Insiders_LTSC : ${getFFN} : ${getBuild}`);
         } else {
-            console.log(` : ${getFFN} : ${getBuild}`);
+            console.log(`Insiders_LTSC : ${getFFN} : ${getBuild}`);
         };
     });
 });
