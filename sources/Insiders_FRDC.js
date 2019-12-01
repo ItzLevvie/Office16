47a32094d691fbb1259d4e5db66c69dc3e9d477d
ItzLevvie <rsajeey@hotmail.com>
2019-12-01 18:03:10 +0000

    8

--- sources/Insiders_FRDC.js
+++ sources/Insiders_FRDC.js
@@ -1,16 +1,16 @@
 const https = require("https");
 
-const getFFN = "b61285dd-d9f7-41f2-9757-8f61cba4e9c8";
-const getBuild = "16.0.12307.20000";
+const getFFN = "b8f9b850-328d-4355-9145-c59439a0c4cf";
+const getBuild = "16.0.11929.20494";
 
 https.get(`https://mrodevicemgr.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=${getFFN}`, (getResponse) => {
     getResponse.on("data", (getData) => {
         if (getResponse.statusCode === 200) {
             const getParsedData = JSON.parse(getData);
             const getBuild = getParsedData.AvailableBuild;
-            console.log(` : ${getFFN} : ${getBuild}`);
+            console.log(`Insiders_FRDC : ${getFFN} : ${getBuild}`);
         } else {
-            console.log(` : ${getFFN} : ${getBuild}`);
+            console.log(`Insiders_FRDC : ${getFFN} : ${getBuild}`);
         };
     });
 });
