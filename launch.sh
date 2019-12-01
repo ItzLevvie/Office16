4dcfc229cdda1acba103b358388803c0693fddc5
ItzLevvie <rsajeey@hotmail.com>
2019-12-01 17:24:33 +0000

    3

---
+++ launch.sh
@@ -0,0 +1,21 @@
+{
+    node sources\1.js
+    node sources\2.js
+    node sources\3.js
+    node sources\4.js
+    node sources\5.js
+    node sources\6.js
+    node sources\7.js
+    node sources\8.js
+    node sources\9.js
+    node sources\10.js
+    node sources\11.js
+    node sources\12.js
+    node sources\13.js
+    node sources\14.js
+    node sources\15.js
+    node sources\16.js
+    node sources\17.js
+    node sources\18.js
+    node sources\19.js
+} > defconfig
