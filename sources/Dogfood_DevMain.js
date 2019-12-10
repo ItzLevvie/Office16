aeb67dd5909254fb819d2821418c5b43c757a4f1
ItzLevvie <rsajeey@hotmail.com>
2019-12-10 20:09:03 +0000

    28

--- sources/Dogfood_DevMain.js
+++ sources/Dogfood_DevMain.js
@@ -1,7 +1,7 @@
 const https = require("https");
 
 const getFFN = "ea4a4090-de26-49d7-93c1-91bff9e53fc3";
-const getBuild = "16.0.12407.20000";
+const getBuild = "16.0.12409.20002";
 
 https.get(`https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=${getFFN}`, (getResponse) => {
     getResponse.on("data", (getData) => {
