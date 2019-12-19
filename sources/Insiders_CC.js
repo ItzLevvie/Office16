d563aed8893643b2105d5ec11436d99c9b3bf1cf
ItzLevvie <rsajeey@hotmail.com>
2019-12-19 21:43:17 +0000

    31

--- sources/Insiders_CC.js
+++ sources/Insiders_CC.js
@@ -3,7 +3,7 @@
 const https = require("https");
 
 const getFFN = "64256afe-f5d9-4f86-8936-8840a6a4f5be";
-const getBuild = "16.0.12325.20172";
+const getBuild = "16.0.12325.20214";
 
 https.get(`https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=${getFFN}`, (getResponse) => {
     getResponse.on("data", (getData) => {
